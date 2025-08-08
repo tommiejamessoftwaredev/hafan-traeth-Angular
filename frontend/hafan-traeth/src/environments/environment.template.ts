7098edb90633c8df3b4612e3d7e8ee80548e735c
tommiejames95 <tommiejames.softwaredev@gmail.com>
2025-08-08 11:47:10 +0100

    update gitignore

---
+++ frontend/hafan-traeth/src/environments/environment.template.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  // Google Maps API Key - Get yours at: https://console.cloud.google.com/apis/credentials
+  // Enable Maps JavaScript API and Geocoding API for your key
+  // For production, restrict the key to your domain
+  googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY_HERE',
+  apiUrl: 'http://localhost:7071/api', // Local Azure Functions URL
+  bookingComUrl: 'https://www.booking.com/hotel/gb/hafan-traeth.en-gb.html',
+  bookingComReviewsUrl: 'https://www.booking.com/hotel/gb/hafan-traeth.en-gb.html#tab-reviews',
+  airbnbUrl: 'https://www.airbnb.co.uk/rooms/920441523710400719',
+  airbnbReviewsUrl: 'https://www.airbnb.co.uk/rooms/920441523710400719/reviews',
+  icalUrl: 'https://ical.booking.com/v1/export?t=YOUR_ICAL_TOKEN_HERE'
+};
